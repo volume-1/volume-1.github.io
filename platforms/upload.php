571b11215cf8cb1ca7db24bedcae79e7d1e32b98
volume-1 <57197436+volume-1@users.noreply.github.com>
2021-02-04 15:23:07 -0500

    Create upload.php

---
+++ platforms/upload.php
@@ -0,0 +1,10 @@
+<?php    
+define('UPLOAD_DIR', 'platforms/images/');
+$img = $_POST['imgBase64'];
+$img = str_replace('data:image/png;base64,', '', $img);
+$img = str_replace(' ', '+', $img);
+$data = base64_decode($img);
+$file = UPLOAD_DIR . uniqid() . '.png';
+$success = file_put_contents($file, $data);
+print $success ? $file : 'Unable to save the file.';
+?> 
